006b42ab9034528ef63eeeb35ce1b4b4e0072ff7
Mathieu Villion <mvillion@gmail.com>
2025-05-26 14:21:58 +0200

    Update tinybinmat_utils.h

--- tinybinmat_utils.h
+++ tinybinmat_utils.h
@@ -1,7 +1,7 @@
 #if !defined(_TINYBITMAT_UTILS)
 #define _TINYBITMAT_UTILS
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#include <numpy/arrayobject.h>
+#include "numpy/arrayobject.h"
 #include "numpy/ndarraytypes.h"
 
 extern void *failure(PyObject *type, const char *message);
